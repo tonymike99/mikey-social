f524ff0718cfbbb1a72388234c9982032783e31f
Tony <tonymike99@gmail.com>
2022-10-17 23:34:06 +0530

    added custom hook for fetching data using axios

--- src/hooks/custom/index.js
+++ src/hooks/custom/index.js
@@ -1,2 +1,3 @@
 export { useDocumentTitle } from "./useDocumentTitle";
 export { useScroll } from "./useScroll";
+export { useAxios } from "./useAxios";
---
+++ src/hooks/custom/useAxios.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useAxios = (params) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const res = await axios.request(params);
+      setResponse(res.data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return { response, error, loading };
+};
+
+export { useAxios };
