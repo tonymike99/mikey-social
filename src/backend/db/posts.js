b3fbb891efe2088058622d89fc17d586fc4e59b0
Tony <tonymike99@gmail.com>
2022-10-17 23:20:09 +0530

    Added dummy posts data

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -10,26 +10,78 @@ export const posts = [
   {
     _id: uuid(),
     content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+      "I'm living a dream I never want to wake up from.”  “I'm living a dream I never want to wake up from.",
     likes: {
-      likeCount: 0,
+      likeCount: 4545093,
       likedBy: [],
       dislikedBy: [],
     },
-    username: "adarshbalika",
+    username: "cristiano_ronaldo",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
   {
     _id: uuid(),
     content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+      "I have fun like a child in the street. When the day comes when I'm not enjoying it, I will leave football.",
     likes: {
-      likeCount: 0,
+      likeCount: 3545041,
       likedBy: [],
       dislikedBy: [],
     },
-    username: "shubhamsoni",
+    username: "lionel_messi",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    content:
+      "Never give up. Today is hard tomorrow will be worse. But the day after tomorrow will be sunshine.",
+    likes: {
+      likeCount: 2847049,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "virat_kohli",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    content:
+      "Cricket is not gender biased. It isn't that men's cricket is different and women's a different one.",
+    likes: {
+      likeCount: 317092,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "mithali_raj",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    content:
+      "The more you praise and celebrate your life, the more there is in life to celebrate.",
+    likes: {
+      likeCount: 2100444,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "oprah_winfrey",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    content:
+      "The more you praise and celebrate your life, the more there is in life to celebrate.",
+    likes: {
+      likeCount: 1113954,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "indra_nooyi",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
