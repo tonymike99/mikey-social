1e87e3a77cb7e660a8b926e0cb89cf5feb8da327
Tony <tonymike99@gmail.com>
2022-10-23 18:10:44 +0530

    added providers to index

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { makeServer } from "./server";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, AuthProvider } from "./hooks/context/index";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
