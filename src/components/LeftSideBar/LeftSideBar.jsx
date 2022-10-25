18eb44113f55680f43808af9e11afeabf6f12e4e
Tony <tonymike99@gmail.com>
2022-10-25 14:28:13 +0530

    added left sidebar

---
+++ src/components/LeftSideBar/LeftSideBar.jsx
@@ -0,0 +1,32 @@
+import "./LeftSideBar.css";
+import { Fragment } from "react";
+import { NavLink } from "react-router-dom";
+
+function LeftSideBar() {
+  const linkDetails = [
+    { to: "/", name: "Home", icon: "fa-home" },
+    { to: "/explore", name: "Explore", icon: "fa-compass" },
+    { to: "/profile", name: "Profile", icon: "fa-user" },
+    { to: "/settings", name: "Settings", icon: "fa-gear" },
+  ];
+
+  return (
+    <aside className="left-sidebar">
+      <ul className="list list-vertical">
+        {linkDetails.map((linkDetail) => (
+          <Fragment key={linkDetail.to}>
+            <li>
+              <NavLink className="styled-link" to={`${linkDetail.to}`}>
+                <i className={`fa-solid ${linkDetail.icon} fa-lg`}></i>
+                {`${linkDetail.name}`}
+              </NavLink>
+            </li>
+            <hr className="hr-thin" />
+          </Fragment>
+        ))}
+      </ul>
+    </aside>
+  );
+}
+
+export { LeftSideBar };
