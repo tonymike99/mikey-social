5d332adcb5ec9a89d13ff5df9ca84b5cbe917161
Tony <tonymike99@gmail.com>
2022-11-21 01:37:15 +0530

    added the suggestions component

--- src/components/Suggestions/Suggestions.jsx
+++ src/components/Suggestions/Suggestions.jsx
@@ -1,7 +1,15 @@
 import { ProfileHeader } from "../index";
 
 function Suggestions({ users }) {
-  return users.map((user) => <ProfileHeader key={user._id} user={user} />);
+  return (
+    <>
+      <h2 className="text-center margin-bottom-2">Suggestions</h2>
+
+      {users.map((user) => (
+        <ProfileHeader key={user._id} user={user} />
+      ))}
+    </>
+  );
 }
 
 export { Suggestions };
