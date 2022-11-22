48b83bc165c40916cb1ad95776ad040b4bc77ca7
Tony <tonymike99@gmail.com>
2022-11-23 00:50:03 +0530

    added all routes to the App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { PrivateRoute, RestrictedRoute } from "./auth/index";
 import { Header, Footer } from "./components/index";
+import {
+  Auth,
+  Home,
+  Explore,
+  Profile,
+  SearchResults,
+  PageNotFound,
+} from "./pages/index";
 
 function App() {
   return (
     <>
       <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<PageNotFound />} />
+
+        <Route element={<RestrictedRoute />}>
+          <Route path="/auth" element={<Auth />} />
+        </Route>
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/profile/:username" element={<Profile />} />
+          <Route path="/search-results/:username" element={<SearchResults />} />
+        </Route>
+      </Routes>
+
       <Footer />
     </>
   );
